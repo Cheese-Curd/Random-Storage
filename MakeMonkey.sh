66883b36e2f10705886b62f18d36a8b4d6a01866
Eliana <51841108+Cheese-Curd@users.noreply.github.com>
2025-10-16 12:37:50 -0400

    Slightly more working

--- MakeMonkey.sh
+++ MakeMonkey.sh
@@ -1,6 +1,6 @@
-wget https://raw.githubusercontent.com/Cheese-Curd/Random-Storage/refs/heads/main/monkey.jpeg
-while true; do
-	cp monkey.jpeg ~/Desktop/
-	xdg-open monkey.jpeg
-done
+#!/bin/bash
+wget -O monkey.jpeg https://raw.githubusercontent.com/Cheese-Curd/Random-Storage/refs/heads/main/monkey.jpeg
 
+while true; do
+  cp monkey.jpeg ~/Desktop/
+done
