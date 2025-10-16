02093179d62a236d833adb2f8f3d4c00ff3f661e
Eliana <51841108+Cheese-Curd@users.noreply.github.com>
2025-10-16 12:44:45 -0400

    Makes copies

--- MakeMonkey.sh
+++ MakeMonkey.sh
@@ -2,5 +2,12 @@
 wget -O monkey.jpeg https://raw.githubusercontent.com/Cheese-Curd/Random-Storage/refs/heads/main/monkey.jpeg
 
 while true; do
-  cp monkey.jpeg ~/Desktop/
+  i=1
+  dest=~/Desktop/monkey_$i.jpeg
+  while [[ -e "$dest" ]]; do
+    ((i++))
+    dest=~/Desktop/monkey_$i.jpeg
+  done
+  cp monkey.jpeg "$dest"
+  sleep 1
 done
