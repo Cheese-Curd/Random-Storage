7fc04760464fe5f7cf249bc97c74f2413b681649
Eliana <51841108+Cheese-Curd@users.noreply.github.com>
2025-10-16 11:35:10 -0400

    Remove unneeded line

--- MakeMonkey.sh
+++ MakeMonkey.sh
@@ -1,6 +1,6 @@
-chmod +x oops.sh
 wget https://raw.githubusercontent.com/Cheese-Curd/Random-Storage/refs/heads/main/monkey.jpeg
 while true; do
 	cp monkey.jpeg ~/Desktop/
 	xdg-open monkey.jpeg
 done
+
